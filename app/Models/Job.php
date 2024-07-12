d720e316afc0e9c6d35f034f0aec0241a41e09e1
washingtoneaura <aurawashingtone@gmail.com>
2024-07-12 13:12:04 +0300

    Baby Steps

---
+++ app/Models/Job.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Job {
+    public static function all(): array {
+        return [
+            [
+                'id' => '1',
+                'title' => 'Manager',
+                'salary' => '60, 000'
+            ],
+            [
+                'id' => '2',
+                'title' => 'Director',
+                'salary' => '50, 000'
+            ],
+            [
+                'id' => '3',
+                'title' => 'Programmer',
+                    'salary' => '45, 000'
+            ],
+            [
+                'id' => '4',
+                'title' => 'Designer',
+                    'salary' => '40, 000'
+            ]
+        ];
+    }
+
+    public static function find(int $id): array {
+        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+        if (! $job) {
+            abort(404);
+        }
+
+        return $job;
+
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,75 +1,25 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
 
 Route::get('/', function () {
     return view('home', [
         'greeting' => 'Hello there',
         'name' => 'Washingtone Aura',
-        
+
     ]);
 });
 
 Route::get('/jobs', function () {
     return view('jobs', [
-    'jobs' => [
-            [
-                'id' => '1',
-                'title' => 'Manager',
-               'salary' => '60, 000'
-            ],
-            [   
-                'id' => '2',
-                'title' => 'Director',
-                'salary' => '50, 000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'Programmer',
-                 'salary' => '45, 000'
-            ],
-            [
-                'id' => '4',
-                'title' => 'Designer',
-                 'salary' => '40, 000'
-            ]
-        ]
+    'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => '1',
-            'title' => 'Manager',
-            'salary' => '60, 000'
-        ],
-        [   
-            'id' => '2',
-            'title' => 'Director',
-            'salary' => '50, 000'
-        ],
-        [
-            'id' => '3',
-            'title' => 'Programmer',
-                'salary' => '45, 000'
-        ],
-        [
-            'id' => '4',
-            'title' => 'Designer',
-                'salary' => '40, 000'
-        ]
-    ];
-
-    //Arr::first($jobs, function($job) use ($id){
-        //return $job['id'] == $id;
-    //});
-    //dd($id);
-
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-    //dd($job);
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
